07e6645535d98b9711e192744577f5baef465112
yoonah5959 <yoonah5959@gmail.com>
2022-10-09 18:06:31 +0900

    hilt 기타등등 디펜던시 추가

--- app/src/main/java/heenu/moon/yoon_memo_android/MainActivity.kt
+++ app/src/main/java/heenu/moon/yoon_memo_android/MainActivity.kt
@@ -3,19 +3,12 @@ package heenu.moon.yoon_memo_android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import heenu.moon.yoon_memo_android.screen.HomeScreen
 import heenu.moon.yoon_memo_android.ui.theme.Yoon_memo_androidTheme
-import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,27 +16,15 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             Yoon_memo_androidTheme {
-
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    HomeScreen()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Yoon_memo_androidTheme {
-        Greeting("Android")
-    }
-}
